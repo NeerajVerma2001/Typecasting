91381459e2ff22e59bb23f48fa6f8b8f0400213f
Neeraj Verma <113288731+NeerajVerma2001@users.noreply.github.com>
2023-06-07 22:03:50 +0530

    added A2.java and B2Driver.java
    Upcasting program

---
+++ A2.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class A2 
+{
+	String car="BMW";
+}
+class B2 extends A2
+{
+	String bike="hayabusa";
+}
+
---
+++ B2Driver.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class B2Driver 
+{
+		public static void main(String[] args) 
+		{
+			A2 a=new B2();
+			System.out.println(a.car);
+			
+			B2 b=new B2();
+			System.out.println(b.bike);
+		}
+	
+
+}
